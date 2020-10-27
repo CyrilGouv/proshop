d986356708e62f51f68f4516b06121339c735ed0
Cyril Gouverneur <cyril@imac-de-cyril.home>
2020-10-27 11:32:19 +0100

    Add Product Details Reducer & Action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -24,14 +24,14 @@ const HomeScreen = () => {
                     ? <Loader />
                     : error 
                         ? <Message variant="danger">{ error }</Message> 
-                        : 
-                        <Row>
-                            { products.map(product => (
-                                <Col key={ product._id } sm={12} md={5} lg={4} xl={3}>
-                                    <Product product={ product } />
-                                </Col>
-                            )) }
-                        </Row>
+                    : 
+                    <Row>
+                        { products.map(product => (
+                            <Col key={ product._id } sm={12} md={5} lg={4} xl={3}>
+                                <Product product={ product } />
+                            </Col>
+                        )) }
+                    </Row>
             }
         </>
     )
